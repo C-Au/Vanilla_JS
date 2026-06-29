62d6487568340f03de4e16ec95581ba30b4acb9d
C-Au <aucolin@hotmail.com>
2026-06-28 21:50:07 -0700

    Implement checkNumber function and add event listener for button click

--- 1_Project/script.js
+++ 1_Project/script.js
@@ -1,3 +1,7 @@
 const buttonClick = document.getElementById("button");
 const resultDisplay = document.getElementById("numberResult");
 const userNumber = document.getElementById("numberInput");
+
+function checkNumber() {};
+
+buttonClick.addEventListener("click", checkNumber);
