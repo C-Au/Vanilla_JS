43953dedf037b4bbc90efa619fac2241acc3b251
C-Au <aucolin@hotmail.com>
2026-06-29 21:25:32 -0700

    Refactor checkNumber function to improve number validation and output messages

--- 1_Project/script.js
+++ 1_Project/script.js
@@ -5,6 +5,15 @@ const userNumber = document.getElementById("numberInput");
 function checkNumber() {
   const userNum = Number(userNumber.value);
   if (Number.isNaN(userNum)) {
+    resultDisplay.textContent = "This is not a number.";
+  } else if (userNum === 0) {
+    resultDisplay.textContent = "This number is zero.";
+  } else if (userNum < 0) {
+    resultDisplay.textContent = "This number is negative.";
+  } else if (userNum % 2 === 0) {
+    resultDisplay.textContent = "This is an even number.";
+  } else {
+    resultDisplay.textContent = "This is an odd number.";
   }
 }
 
