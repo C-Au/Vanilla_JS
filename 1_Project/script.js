66a8f94868304ceae432b6a34f87a77b7fd4f540
C-Au <aucolin@hotmail.com>
2026-06-29 20:38:06 -0700

    Update input label for clarity and implement number conversion in checkNumber function

--- 1_Project/script.js
+++ 1_Project/script.js
@@ -2,6 +2,8 @@ const buttonClick = document.getElementById("button");
 const resultDisplay = document.getElementById("numberResult");
 const userNumber = document.getElementById("numberInput");
 
-function checkNumber() {}
+function checkNumber() {
+  const userNum = Number(userNumber.value);
+}
 
 buttonClick.addEventListener("click", checkNumber);
