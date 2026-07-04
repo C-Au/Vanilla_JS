8e843e8f49d704e416e9f4028cc5771ace27e756
C-Au <aucolin@hotmail.com>
2026-07-03 20:43:15 -0700

    Update input validation to require a minimum of 4 characters for list items

--- 3_Project_To_Do_List/script.js
+++ 3_Project_To_Do_List/script.js
@@ -3,7 +3,7 @@ const input = document.getElementById("inputField");
 const userList = document.getElementById("listInput");
 
 button.addEventListener("click", () => {
-  if (input.value.length === 0) {
+  if (input.value.length < 4) {
     return;
   }
   let li = document.createElement("li");
