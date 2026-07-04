2799b2bb18296a23da708038fd34472faabe41ca
C-Au <aucolin@hotmail.com>
2026-07-03 21:10:39 -0700

    Add HTML structure and JavaScript functionality for To Do List application

---
+++ 4_Project_To_Do_List_Adv/script.js
@@ -0,0 +1,13 @@
+const button = document.getElementById("button");
+const input = document.getElementById("inputField");
+const userList = document.getElementById("listInput");
+
+button.addEventListener("click", () => {
+  if (input.value.length < 4) {
+    return;
+  }
+  let li = document.createElement("li");
+  li.textContent = input.value;
+  userList.appendChild(li);
+  input.value = "";
+});
