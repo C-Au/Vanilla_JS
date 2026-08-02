3f03cac086eb82e0673119252d34a4b2231bb95d
C-Au <aucolin@hotmail.com>
2026-08-01 19:38:41 -0700

    Fix script.js and index.html: update input field ID from textField to nameField for consistency

--- 5_Product List App/script.js
+++ 5_Product List App/script.js
@@ -1,4 +1,4 @@
-const textInput = document.getElementById("textField");
+const nameInput = document.getElementById("nameField");
 const numberInput = document.getElementById("numberField");
 const productList = document.getElementById("listInput");
 const filterBtn = document.getElementById("filterButton");
@@ -11,13 +11,15 @@ let products = [
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (textInput.value.length === 0) {
+  if (nameInput.value.length === 0) {
     return;
   }
   if (numberInput.value < 0) {
     return;
   }
-  productPost(textInput.value, numberInput.value);
-  textInput.value = "";
+  productPost(nameInput.value, numberInput.value);
+  nameInput.value = "";
   numberInput.value = "";
 });
+
+const productPost = (productName, productNumber) => {};
